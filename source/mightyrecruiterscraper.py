a2d973e0ed7427ffad91c39779df082e6a8e13cb
Nikunj <nikunjbjj@gmail.com>
2019-05-05 15:33:08 -0700

    Adding selenium file

--- source/mightyrecruiterscraper.py
+++ source/mightyrecruiterscraper.py
@@ -1,112 +1,36 @@
-# from http.cookiejar import LWPCookieJar
-#
-# import mechanize
-# import cookiejar
-# from bs4 import BeautifulSoup
-# import html2text
-# import requests
-# from lxml import html
-#
-#
-#
-# # # Browser
-# # br = mechanize.Browser()
-# #
-# # # Cookie Jar
-# # cj = LWPCookieJar()
-# # br.set_cookiejar(cj)
-# #
-# # # Browser options
-# # br.set_handle_equiv(True)
-# # br.set_handle_gzip(True)
-# # br.set_handle_redirect(True)
-# # br.set_handle_referer(True)
-# # br.set_handle_robots(False)
-# # br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-# #
-# # br.addheaders = [('User-agent', 'Chrome')]
-# #
-# # # The site we will navigate into, handling it's session
-# # br.open('https://recruiter.mightyrecruiter.com/accounts')
-# #
-# # # View available forms
-# # for idx, f in enumerate(br.forms()):
-# #
-# #     print(idx, f)
-# #
-# # # Select the second (index one) form (the first form is a search query box)
-# # br.select_form(nr=1)
-# #
-# # # User credentials
-# # br.form['UserName'] = 'sreetasai@gmail.com'
-# # br.form['Password'] = 'Wrevie123'
-# #
-# # # Login
-# # br.submit()
-# #
-# # print(br.open('https://www.mightyrecruiter.com/resumes').read())
-#
-#
-# session_requests = requests.session()
-# login_url = "https://recruiter.mightyrecruiter.com/accounts"
-# result = session_requests.get(login_url, headers={'User-Agent': 'Mozilla/5.0'})
-# tree = html.fromstring(result.text)
-# payload = {'UserName': 'sreetasai@gmail.com', 'Password': 'Wrevie123'}
-# response = session_requests.post(
-#     login_url,
-#     data=payload,
-#     headers={'User-Agent': 'Mozilla/5.0'}
-# )
-# #print(response.raw, response.text)
-# # url = 'http://recruiter.mightyrecruiter.com/resumes/Get?jobID=0&page_number=1&page_size=20&search_on=jt&search_string=software&location=&sortBy=score&radius=30&min_experience=0&max_experience=1200&mned=NA&min_age=1&max_age=5000&hide_viewed_resume=false&hide_reviewed_resume=false&documentId=0&similarResumeSearch=false'
-# # result = session_requests.get(
-# #     url,
-# #     headers={'User-Agent': 'Mozilla/5.0'}
-# # )
-# # import pdb; pdb.set_trace()
-# # print(result, result.content)
-# #
-# # tree = html.fromstring(result.content)
-# # import pdb; pdb.set_trace()
-# # print(dir(tree))
-# # bucket_names = tree.xpath("//div[@class='repo-list--repo']/a/text()")
-# #
-# # print(bucket_names)
-#
-# import urllib.request, json
-# # import requests
-# # import json
-# # # with urllib.request.urlopen(url, data={'User-Agent': 'Mozilla/5.0'}) as url:
-# # #     data = json.loads(url.read().decode())
-# # #     print(data)
-# #
-# # res = requests.post(url,
-# #                  headers={'User-Agent': 'Mozilla/5.0', 'application/json'}
-# #                  )
-# # import pdb; pdb.set_trace()
-# # # dataform = str(r).strip("'<>() ").replace('\'', '\"')
-# # struct = json.loads(dataform)
-# # print(struct)
-#
-# # res = urllib.request.urlopen('urllib')
-# # res_body = res.read()
-# #
-# # # https://docs.python.org/3/library/json.html
-# # j = json.loads(res_body.decode("utf-8"))
-# # print(j)
-
 import requests
+import json
+
+session_requests = requests.session()
+login_url = "https://recruiter.mightyrecruiter.com/login"
+payload = {
+    "UserName": "Sreetasai@gmail.com",
+    "Password": "Wrevie123",
+    "IsPersistent": ""
+}
 
 headers = {
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'en-US,en;q=0.9',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
     'accept': 'application/json, text/plain, */*',
-    'referer': 'https://recruiter.mightyrecruiter.com/resumes',
     'authority': 'recruiter.mightyrecruiter.com',
-    'cookie': 'optimizelyEndUserId=oeu1556776003338r0.03323844251430619; ajs_group_id=null; _mwr_visitortype=true; ajs_anonymous_id=%226df02273-b0d5-4c1d-baa2-18975972077a%22; G_ENABLED_IDPS=google; _ga=GA1.2.1517422009.1556776004; _gid=GA1.2.914979616.1556776004; __insp_wid=473465058; __insp_nv=true; __insp_targlpu=aHR0cHM6Ly9yZWNydWl0ZXIubWlnaHR5cmVjcnVpdGVyLmNvbS9hY2NvdW50cz9SZXR1cm5Vcmw9JTJGZGFzaGJvYXJk; __insp_targlpt=TG9naW4%3D; __insp_norec_sess=true; vstr=59a4f63f-b352-4c12-8819-105728c5b9b5; vsuid=2de64596-fed5-4fcd-acde-dc3950709d54; ref=13128; vsutms=1f6cd565-ccd4-4301-953f-cf98aa3d2065#59a4f63f-b352-4c12-8819-105728c5b9b5#2de64596-fed5-4fcd-acde-dc3950709d54#1556776004#||||; pnctest=1; JobTap=iunPYEODvNizWRz5lwR1WhbnSPe-UqJnXapPZZeoLBSI-jEmxTUO61aza_slM4MhOp7EaRqOWlo4hHTvi4eVMbp3iPyLUpZwgzWvAGJYAhhj8etbswauIfxYKQqixwMNAot0BesfYn9jIAYPwPeBFHAM1WZe55F9ERbwIDVbJnV7LqL3xVpT-aTWYmQg6pQ6BDWskr5S8VlDzwnM7z5pYHfLOp_XNmeJzn7uOr4tuX6KvtsTbfRQiL2OKmDRKOq58BWK1nH8Rnwt6ne6TQ4F4MMlat-V4tyXnn-5hn0Bl0VGPIGVoyv9aWC_uoNirgqI8z_-F-3MWDBdA7-Es7xNQF1YcMHD1nVf6nfshBgbpopwcQp4CbjYf7bw25aHUxpmmynosOqpkDq5kgtWIip7QQbN5TXLpmuS_r1-0P84X5LJQxccoxMtSMgBM0Z-yX2dC44eEPJywoLkJLPs077K6Sl2tk7-Hv0gLqix1SVPO_XAa5mfCukVDX2nUxhPSz4gK7ZxnCkdU5wtsJhP2deX20mnIDSjfG60SEq4zkfGoQ3Hmj_YlMftAgu6LxyJLkLmsRYqcfbJ9mOpjKQ2Y-AjeEyt05JASxJlb7UR50x6Gdl5TABepFL6vdyAV_jwPIh0; ajs_user_id=%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22; _mwr_trk=%7B%22requiresignin%22%3A%22False%22%2C%22userid%22%3A%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22email%22%3A%22sreetasai%40gmail.com%22%2C%22firstname%22%3A%22Sreeta%22%2C%22lastname%22%3A%22Gorripaty%22%2C%22cname%22%3A%22Wrevie%22%7D; __ssid=30ab9b5122ad16a2ccb743d02eab455; mp_0568fc1726576866d45cfa17ad4837b9_mixpanel=%7B%22distinct_id%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22%24device_id%22%3A%20%2216a7714a9122d7-06586eb482efa3-366e7e04-13c680-16a7714a9133b6%22%2C%22mp_lib%22%3A%20%22Segment%3A%20web%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%2C%22%24user_id%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22mp_name_tag%22%3A%20%22sreetasai%40gmail.com%22%2C%22userId%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22Platform%22%3A%20%22Web%22%2C%22Product%22%3A%20%22Mightyworks%20Recruiter%22%2C%22Login%20Status%22%3A%20%22TRUE%22%2C%22id%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22%24email%22%3A%20%22sreetasai%40gmail.com%22%7D; __insp_slim=1556776949800',
+    'referer': 'https://recruiter.mightyrecruiter.com/accounts',
+    'upgrade-insecure-requests': '1',
+    'origin': 'https://recruiter.mightyrecruiter.com',
+    'content-type': 'application/x-www-form-urlencoded'
 }
 
+result = session_requests.post(
+    login_url,
+    data=payload,
+    headers=headers
+)
+
+print ("Login Status code: " + str(result.status_code))
+
+#### Get List of resumes ####
+url = "https://recruiter.mightyrecruiter.com/resumes/Get"
 params = (
     ('jobID', '0'),
     ('page_number', '1'),
@@ -127,62 +51,39 @@ params = (
     ('similarResumeSearch', 'false'),
 )
 
-response = requests.get('https://recruiter.mightyrecruiter.com/resumes/Get', headers=headers, params=params)
-json_response = response.json()
-# import pdb; pdb.set_trace()
+resumes_list_result = session_requests.get(url, headers=headers, params=params)
+print("Resumes status code: " + str(resumes_list_result.status_code))
+
+json_response = resumes_list_result.json()
+print(json_response)
 
 
-import requests
-import json
 error_file = 'errorfile'
 res_id = 'someid'
 res_link = 'somepath'
+### Loop over the resumes data and fetch the resume HTML ####
 for idx, res in enumerate(json_response['ResultList'][:3]):
     try:
         res_id = res.get('ID', 'someid')
+        print("ID: ", res_id)
         res_link = res.get('FilePathURI_HTML', 'somepath')
-
-        resume_headers = {
-            'authority': 'recruiter.mightyrecruiter.com',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-            'referer': 'https://recruiter.mightyrecruiter.com/resumes',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-US,en;q=0.9',
-            'cookie': 'optimizelyEndUserId=oeu1556776003338r0.03323844251430619; ajs_group_id=null; _mwr_visitortype=true; ajs_anonymous_id=%226df02273-b0d5-4c1d-baa2-18975972077a%22; G_ENABLED_IDPS=google; _ga=GA1.2.1517422009.1556776004; _gid=GA1.2.914979616.1556776004; __insp_wid=473465058; __insp_nv=true; __insp_targlpu=aHR0cHM6Ly9yZWNydWl0ZXIubWlnaHR5cmVjcnVpdGVyLmNvbS9hY2NvdW50cz9SZXR1cm5Vcmw9JTJGZGFzaGJvYXJk; __insp_targlpt=TG9naW4%3D; __insp_norec_sess=true; vstr=59a4f63f-b352-4c12-8819-105728c5b9b5; vsuid=2de64596-fed5-4fcd-acde-dc3950709d54; ref=13128; vsutms=1f6cd565-ccd4-4301-953f-cf98aa3d2065#59a4f63f-b352-4c12-8819-105728c5b9b5#2de64596-fed5-4fcd-acde-dc3950709d54#1556776004#||||; pnctest=1; JobTap=iunPYEODvNizWRz5lwR1WhbnSPe-UqJnXapPZZeoLBSI-jEmxTUO61aza_slM4MhOp7EaRqOWlo4hHTvi4eVMbp3iPyLUpZwgzWvAGJYAhhj8etbswauIfxYKQqixwMNAot0BesfYn9jIAYPwPeBFHAM1WZe55F9ERbwIDVbJnV7LqL3xVpT-aTWYmQg6pQ6BDWskr5S8VlDzwnM7z5pYHfLOp_XNmeJzn7uOr4tuX6KvtsTbfRQiL2OKmDRKOq58BWK1nH8Rnwt6ne6TQ4F4MMlat-V4tyXnn-5hn0Bl0VGPIGVoyv9aWC_uoNirgqI8z_-F-3MWDBdA7-Es7xNQF1YcMHD1nVf6nfshBgbpopwcQp4CbjYf7bw25aHUxpmmynosOqpkDq5kgtWIip7QQbN5TXLpmuS_r1-0P84X5LJQxccoxMtSMgBM0Z-yX2dC44eEPJywoLkJLPs077K6Sl2tk7-Hv0gLqix1SVPO_XAa5mfCukVDX2nUxhPSz4gK7ZxnCkdU5wtsJhP2deX20mnIDSjfG60SEq4zkfGoQ3Hmj_YlMftAgu6LxyJLkLmsRYqcfbJ9mOpjKQ2Y-AjeEyt05JASxJlb7UR50x6Gdl5TABepFL6vdyAV_jwPIh0; ajs_user_id=%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22; _mwr_trk=%7B%22requiresignin%22%3A%22False%22%2C%22userid%22%3A%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22email%22%3A%22sreetasai%40gmail.com%22%2C%22firstname%22%3A%22Sreeta%22%2C%22lastname%22%3A%22Gorripaty%22%2C%22cname%22%3A%22Wrevie%22%7D; __ssid=30ab9b5122ad16a2ccb743d02eab455; mp_0568fc1726576866d45cfa17ad4837b9_mixpanel=%7B%22distinct_id%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22%24device_id%22%3A%20%2216a7714a9122d7-06586eb482efa3-366e7e04-13c680-16a7714a9133b6%22%2C%22mp_lib%22%3A%20%22Segment%3A%20web%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%2C%22%24user_id%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22mp_name_tag%22%3A%20%22sreetasai%40gmail.com%22%2C%22userId%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22Platform%22%3A%20%22Web%22%2C%22Product%22%3A%20%22Mightyworks%20Recruiter%22%2C%22Login%20Status%22%3A%20%22TRUE%22%2C%22id%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22%24email%22%3A%20%22sreetasai%40gmail.com%22%7D; _gat=1; __insp_slim=1556778475139',
-        }
-
-
-
-        # resume_params = (
-        #     ('path', res_link),
-        #     ('search_string', 'software'),
-        # )
-
         resume_params = (
             ('path', res_link),
-            ('documentid', '%s' % res_id),
+            ('documentid', res_id),
             ('search_string', 'software'),
         )
-
+        # headers.update({'cookie': 'optimizelyEndUserId=oeu1556771288707r0.9429390521310967; ajs_group_id=null; vstr=7c265542-f036-45a1-873c-9f51968fc7bd; ref=13127; visitinfo=cuofn2UmFz8HqM1qiI+BZcR5/i9CANW9AAHoCMZrIMgxltIrMwZCIVwfJVdw2wMHmV2wOKMqpVJRvnBjGSFSsl5pP/t6R8jDPjq+2CIuBK0SMcl5jbWeQ8IeLLoQNFvqAMmtN7kicJXoODUULQ02Xz7pV9UAdD3g; vsuid=358fae98-214f-47e4-84c3-26954a9d418f; _ga=GA1.2.1765843271.1556771290; _gid=GA1.2.980701982.1556771290; __unam=86b865-16a76ccb5db-57709dee-2; vsutms=1788d846-804c-43c9-b90b-3ed7aee4d747#7c265542-f036-45a1-873c-9f51968fc7bd#a1cee15f-d9e2-40dd-a207-3881f0b27c51#1556771993#google|sem|brand_-_us_-_search|mightyrecruiter|; _gac_UA-3289623-12=1.1556771994.CLii-uK449MCFcdffgodE_oLqg; utmParams=%7C%7C%7C%7C; pnctest=1; G_ENABLED_IDPS=google; _mwr_trk_m={"requiresidentify":"false","requiresignup":"true","userid":"b0a41b67-a4cb-4c80-8330-068425a72ea2","email":"nikunjbjj@gmail.com","firstname":"Nikunj","lastname":"Bajaj","cname":"Wrevie","ctype":"Employer","cwebsite":"wrevie.com","mediapermission":"TRUE","contactbyphonepermission":"TRUE","accountstatus":"Pending","planstatus":null,"noofjobs":null}; _mwr_visitortype=true; IR_gbd=mightyrecruiter.com; IR_5168=1556772248047%7C0%7C1556772248047; __ssid=e3db82b8fe87acf0dfbb35705bb851b; IR_PI=1806628e-9e78-3699-c3e1-cb68a9b2d1c6%7C1556858648047; G_AUTHUSER_H=0; JobTap=l7WYlhcYgOfrOYB3VqVz1F2IE9NFeAdAiCkH93MqtBOvzlZKZ54Wp37baNrVyT_56zQ3s5aGNnZzs18-HTJZNCQIFzqn3bFo-OFz2hAc_9Wq0fQSEGJ4P6BsCbuZtsuIRKBNg4NM33Z2huiBbE3YHuZ-47Xn8-1kTSWmzmOmNB4C1ofQaW8U_CFMRiiXARJaEv_irCZHjZQorkigi2JkcgN0iiAqlG7A0HpFFbgtUWuMGKZGXu3wFhhlfc9umcSrlMdbAUoS5C0-x1Xi8v0Wf3E4-vsx8DNVt_n2rkI5wTWq4Ue3PIZBZoiCpyJzRuqr89CbBv5bAj5sBu7I8WfuQMb8lb1qD77bNp097bXAlhf8uh0400cEilSw2UyX-rzYqH03EnEHrDCqtASg6S6mO54yHtdCPmf0Ol19O52thzLczMkH0NjHyYaG0yrLaYeWOZhpIPKPN8Joe9szQ1SrEExDslmEt4xqEHcsU_J8owZdVvqOWa8yY8Eo7lAEwIKvFcIRjakBrIwH_Jz1sb0aYckljipQla-_WH95E2ZEfEKuUzrtlbFNa9VAeEVISz3mM_-3wb5TJlORpS515fiGZeeCQbXFe_Sm0WdnMnUzeEJOH2aQCNF9KIUjrXZnDU7f; _mwr_trk=%7B%22requiresignin%22%3A%22False%22%2C%22userid%22%3A%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22email%22%3A%22sreetasai%40gmail.com%22%2C%22firstname%22%3A%22Sreeta%22%2C%22lastname%22%3A%22Gorripaty%22%2C%22cname%22%3A%22Wrevie%22%7D; __insp_wid=473465058; __insp_nv=true; __insp_targlpu=aHR0cHM6Ly9yZWNydWl0ZXIubWlnaHR5cmVjcnVpdGVyLmNvbS9yZXN1bWVz; __insp_targlpt=UmVzdW1lIERhdGFiYXNl; __insp_norec_sess=true; _gat=1; __insp_slim=1556855841996; ajs_user_id=%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22; ajs_anonymous_id=%22d1d19e5d-6e69-42a4-bc90-a56a0379036f%22; mp_0568fc1726576866d45cfa17ad4837b9_mixpanel=%7B%22distinct_id%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22%24device_id%22%3A%20%2216a76ccb9e01e6-03ab1f48aae2b4-366e7e04-13c680-16a76ccb9e1539%22%2C%22mp_lib%22%3A%20%22Segment%3A%20web%22%2C%22%24search_engine%22%3A%20%22google%22%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.google.com%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.google.com%22%2C%22utm_source%22%3A%20%22google%22%2C%22utm_medium%22%3A%20%22sem%22%2C%22utm_campaign%22%3A%20%22brand_-_us_-_search%22%2C%22utm_term%22%3A%20%22mightyrecruiter%22%2C%22%24user_id%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22mp_name_tag%22%3A%20%22sreetasai%40gmail.com%22%2C%22userId%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22Company%20Name%22%3A%20%22Wrevie%22%2C%22Company%20Type%22%3A%20%22Employer%22%2C%22Number%20Of%20Jobs%22%3A%20null%2C%22Company%20Website%22%3A%20%22wrevie.com%22%2C%22Media%20Permission%20For%20Job%20Board%20Performance%22%3A%20%22TRUE%22%2C%22Contact%20By%20Phone%20Permission%22%3A%20%22TRUE%22%2C%22Account%20Status%22%3A%20%22Pending%22%2C%22Plan%20Status%22%3A%20null%2C%22Platform%22%3A%20%22Web%22%2C%22Product%22%3A%20%22Mightyworks%20Recruiter%22%2C%22Login%20Status%22%3A%20%22TRUE%22%2C%22id%22%3A%20%221637792f-9c61-4e53-b5ea-49ebc8a55b71%22%2C%22%24email%22%3A%20%22sreetasai%40gmail.com%22%2C%22%24first_name%22%3A%20%22Nikunj%22%2C%22%24last_name%22%3A%20%22Bajaj%22%2C%22%24name%22%3A%20%22Nikunj%20Bajaj%22%7D'})
         resume_response = requests.get('https://recruiter.mightyrecruiter.com/resumes/ResumePreview',
-                                       headers=resume_headers,
+                                       headers=headers,
                                        params=resume_params)
-        import pdb; pdb.set_trace()
         with open('data/resume_data_%s_%s' % (idx, res_id), 'w') as f:
             f.write("%s\n" % json.dumps(res))
             f.write(resume_response.text)
     except Exception as e:
         with open(error_file, 'a') as f:
-            f.write("Failed for %s, %s, %s\n" % (idx, res_id, res_link))
-            f.write("%s\n" % e)
+            f.write("Failed for %s, %s, %s, %s\n" % (idx, res_id, res_link, e))
+            # f.write("%s\n" % e)
 
-    # print(resume_response.text)
-
-# # resume_response = requests.get('https://recruiter.mightyrecruiter.com/resumes/ResumePreview?path=zDtiQ%2FmH5rW5%2BiQMGfXDS8TFt8%2BcbnZLHrMgG%2FeRia49Rkh8pHW501ZLksrASY8sIoczEO1jBCFWJV7VYs3RcwyVcp76BL1E%2BEn1OpSjSbVxkdIi2l8ptvOQ6Ji8UGi6dUBq3XnI7eYJA5L5E9qVicY388iDA9Ou1l0C3qSui02%2BnMQxpVk%2FO0bcOi6tObKN3725E2V%2F4ao3wOv1i50gxIOE0OhgjdRtz0O6ZDLV5t%2BaXwKegO74WMxFrHFYKB0DHPC%2BkZ0eqbhn0gTGceLAXL9Mwgw16bglPxi7rbmzLuEMgWKefBfqPOYUY78d3Rqr%2FnPX8hSgYJW%2BL0jXXMO56cnWxNAstJphw3ov1P1DfNNKqXvAtW%2BECLJTCOrqDArjvJrSqXrK1EFGIOL1h9WpiI4hUZZhe2ym7TFWK1UgHRye%2FDCF01FAPSmHaXYOfsRGZZWyMPmV39PmWWBtV4oavR4YN%2F%2Btr1RvqPRf2xrrdgx8nx4IHB4d5T7B%2Fjz26bMkWUZDmp71%2FHtDPZojTnRQAw%3D%3D&documentid=92424785&search_string=software',
-# #                                headers=resume_headers)
-# # print(resume_response.json())
 #
 # from html.parser import HTMLParser
 #
@@ -199,7 +100,3 @@ for idx, res in enumerate(json_response['ResultList'][:3]):
 # parser = MyHTMLParser()
 # parser.feed(resume_response.text)
 
-#NB. Original query string below. It seems impossible to parse and
-#reproduce query strings 100% accurately so the one below is given
-#in case the reproduced version is not "correct".
-# response = requests.get('https://recruiter.mightyrecruiter.com/resumes/ResumePreview?path=zDtiQ%2FmH5rW5%2BiQMGfXDS8TFt8%2BcbnZLHrMgG%2FeRia49Rkh8pHW501ZLksrASY8sIoczEO1jBCFWJV7VYs3RcwyVcp76BL1E%2BEn1OpSjSbVxkdIi2l8ptvOQ6Ji8UGi6dUBq3XnI7eYJA5L5E9qVicY388iDA9Ou1l0C3qSui02%2BnMQxpVk%2FO0bcOi6tObKN3725E2V%2F4ao3wOv1i50gxIOE0OhgjdRtz0O6ZDLV5t%2BaXwKegO74WMxFrHFYKB0DHPC%2BkZ0eqbhn0gTGceLAXL9Mwgw16bglPxi7rbmzLuEMgWKefBfqPOYUY78d3Rqr%2FnPX8hSgYJW%2BL0jXXMO56cnWxNAstJphw3ov1P1DfNNKqXvAtW%2BECLJTCOrqDArjvJrSqXrK1EFGIOL1h9WpiI4hUZZhe2ym7TFWK1UgHRye%2FDCF01FAPSmHaXYOfsRGZZWyMPmV39PmWWBtV4oavR4YN%2F%2Btr1RvqPRf2xrrdgx8nx4IHB4d5T7B%2Fjz26bMkWUZDmp71%2FHtDPZojTnRQAw%3D%3D&documentid=92424785&search_string=software', headers=headers)
---
+++ source/test_selenium.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+import os
+
+DRIVER_BIN = os.path.join("", "/Users/cusgadmin/Downloads/chromedriver")
+
+browser = webdriver.Chrome(executable_path=DRIVER_BIN)
+browser.get('https://recruiter.mightyrecruiter.com/login')
+
+username = browser.find_element_by_id("UserName")
+password = browser.find_element_by_id("Password")
+
+username.send_keys("Sreetasai@gmail.com")
+password.send_keys("Wrevie123")
+
+browser.find_element_by_xpath("//*[@id='frmlogin']/div[1]/div[4]/div/button").click()
+
+browser.get('https://recruiter.mightyrecruiter.com/resumes')
+
+browser.find_element_by_id("txtrsjobtitle").send_keys("software")
+
+browser.find_element_by_id("resumeSearch").click()
+
+browser.implicitly_wait(5)
+
+browser.find_element_by_xpath("/html/body/div[1]/div[1]/div/div[2]/div/section/div[2]/div[3]/div[2]/div[3]/a").click()
+import pdb; pdb.set_trace()
+
+# html_text = browser.find_element_by_id("ifrmLoadRsm")
+
+browser.switch_to.frame(browser.find_element_by_id("ifrmLoadRsm"))
+
+html_text = browser.page_source
+
+with open('/Users/cusgadmin/Downloads/resume.html', 'w') as f:
+    f.write(html_text)
+
+
+
+
+# headers = {
+#     'accept-encoding': 'gzip, deflate, br',
+#     'accept-language': 'en-US,en;q=0.9',
+#     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+#     'accept': 'application/json, text/plain, */*',
+#     'authority': 'recruiter.mightyrecruiter.com',
+#     'referer': 'https://recruiter.mightyrecruiter.com/accounts',
+#     'upgrade-insecure-requests': '1',
+#     'origin': 'https://recruiter.mightyrecruiter.com',
+#     'content-type': 'application/x-www-form-urlencoded'
+# }
+#
+#
+#
+# for key, value in enumerate(headers):
+#     capability_key = 'phantomjs.page.customHeaders.{}'.format(key)
+#     webdriver.DesiredCapabilities.PHANTOMJS[capability_key] = value
